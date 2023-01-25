21c1d97e895f8f19e07c29fb71ae59d9cabd9c37
rafaildll <71734050+rafaildll@users.noreply.github.com>
2023-01-25 15:45:26 +0100

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -17,7 +17,7 @@ const FooterCompound = () => (
   <Footer>
     <Branding
       visible={process.env.REACT_APP_BRANDING !== "false"}
-      href="https://triostate.net/impressum"
+      href="https://triostate.net/"
       rel="noopener"
       target="_blank"
     >
